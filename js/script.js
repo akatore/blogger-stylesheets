463e6c34b6c082a279e2a7fea0b01427ca4307d1
akatore <65213016+akatore@users.noreply.github.com>
2023-03-09 19:40:26 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
+// function copyCode() {
+//   // Get the code snippet element
+//   const codeSnippet = document.querySelector('.code-snippet code');
+  
+//   // Create a temporary textarea element
+//   const textarea = document.createElement('textarea');
+//   textarea.value = codeSnippet.innerText;
+  
+//   // Append the textarea element to the body
+//   document.body.appendChild(textarea);
+  
+//   // Select the text inside the textarea element
+//   textarea.select();
+  
+//   // Copy the selected text to the clipboard
+//   document.execCommand('copy');
+  
+//   // Remove the textarea element from the body
+//   document.body.removeChild(textarea);
+  
+//   // Alert the user that the text has been copied
+//   alert('Text copied to clipboard!');
+// }
+
+
+/* JavaScript */
+
 function copyCode() {
   // Get the code snippet element
   const codeSnippet = document.querySelector('.code-snippet code');
